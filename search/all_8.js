df189f9ae976a0b6ceca3f4446c2d24e91267d69
Dan Nechita <dan.nechita@analog.com>
2019-05-13 17:41:23 +0300

    Update documentation to 4e5bc7b

---
+++ search/all_8.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['initialize',['initialize',['../classaditof_1_1_camera.html#a016796dfeb7529dce15d44bcbbaaaab3',1,'aditof::Camera::initialize()'],['../classaditof_1_1_system.html#ae3a5b1520058e66f6ae276ba8ab849dd',1,'aditof::System::initialize()']]],
+  ['intrinsicparameters',['IntrinsicParameters',['../structaditof_1_1_intrinsic_parameters.html',1,'aditof']]],
+  ['intrinsics',['intrinsics',['../structaditof_1_1_camera_details.html#af242b848a9535fc931c48835c85c8e9f',1,'aditof::CameraDetails']]],
+  ['invalid_5fargument',['INVALID_ARGUMENT',['../namespaceaditof.html#a44f136ae2a9a546362d75a5d73443da9af295a0c3e37c94f078e1c5476479132d',1,'aditof']]],
+  ['ip',['ip',['../structaditof_1_1_device_construction_data.html#ac504c85a59520efb970a84d01a0d762b',1,'aditof::DeviceConstructionData']]],
+  ['ir',['IR',['../namespaceaditof.html#ad506ecdfc43f45f45df0cb57ff0a3638a4f74d343f26bbd58a8cbbad2cab6a704',1,'aditof']]],
+  ['irgammacorrection',['irGammaCorrection',['../classaditof_1_1_camera96_tof1_specifics.html#ac120b9f64a57d2c6fac28871df142fda',1,'aditof::Camera96Tof1Specifics::irGammaCorrection()'],['../classaditof_1_1_camera_chicony_specifics.html#aaaa90b6c7e0d9d691e47ecabc8e3ae81',1,'aditof::CameraChiconySpecifics::irGammaCorrection()']]]
+];
---
+++ search/functions_5.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['initialize',['initialize',['../classaditof_1_1_camera.html#a016796dfeb7529dce15d44bcbbaaaab3',1,'aditof::Camera::initialize()'],['../classaditof_1_1_system.html#ae3a5b1520058e66f6ae276ba8ab849dd',1,'aditof::System::initialize()']]],
+  ['irgammacorrection',['irGammaCorrection',['../classaditof_1_1_camera96_tof1_specifics.html#ac120b9f64a57d2c6fac28871df142fda',1,'aditof::Camera96Tof1Specifics::irGammaCorrection()'],['../classaditof_1_1_camera_chicony_specifics.html#aaaa90b6c7e0d9d691e47ecabc8e3ae81',1,'aditof::CameraChiconySpecifics::irGammaCorrection()']]]
+];
